6bf632b3f09c20b3f89d0b7c39068b68c0a3fb93
yvngfloyd <125301624+yvngfloyd@users.noreply.github.com>
2026-03-23 20:44:28 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -110,7 +110,7 @@ export default function App() {
               <div className="hero-badge">Telegram и мессенджер-боты под задачу бизнеса</div>
 
               <h1 className="hero-title">
-                Боты любой сложности
+                Удобные боты
                 <span>для заявок, записи и FAQ</span>
               </h1>
 
