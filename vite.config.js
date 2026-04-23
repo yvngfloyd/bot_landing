fad04177a8b8d84db3e3ad22fcbc8236f0851331
yvngfloyd <125301624+yvngfloyd@users.noreply.github.com>
2026-04-23 11:29:38 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        pricing: resolve(__dirname, "pricing.html"),
+      },
+    },
+  },
 });
